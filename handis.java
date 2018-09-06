20325cdedee7b1418dbc81adb4e988b727af3706
MuhandisAchsani <43022701+MuhandisAchsani@users.noreply.github.com>
2018-09-06 10:12:23 +0700

    Add files via upload

---
+++ handis.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author asus
+ */
+public class handis {
+    
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jml=5;
+        for (int i=1;i<=jml;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
